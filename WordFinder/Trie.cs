82c75a4e7734d2a43f9d7f56847bd045e036d9dc
temobgallardo <62340961+temobgallardo@users.noreply.github.com>
2024-09-27 14:52:48 -0600

    added CountOnStream method to count words in trie on an input stream of chars

--- WordFinder/Trie.cs
+++ WordFinder/Trie.cs
@@ -1,6 +1,8 @@
+using System.Globalization;
+
 namespace WordFinder;
 
-public class Trie
+public class Trie //: ITrie
 {
   private readonly TrieNode _rootNode = new();
 
@@ -37,5 +39,49 @@ public class Trie
     }
 
     return node.IsWord;
+  }
+
+  public bool CountOnStream(string stream, int startSearchIndex, int endSearchIndex, out int stopIndex)
+  {
+    if (startSearchIndex < 0 || startSearchIndex > stream.Length || endSearchIndex < 0 || endSearchIndex > stream.Length)
+    {
+      throw new IndexOutOfRangeException($"Either {nameof(startSearchIndex)} or {nameof(endSearchIndex)} are out of bounds.");
+    }
+
+    var node = _rootNode;
+    stopIndex = 0;
+    for (int i = startSearchIndex; i < endSearchIndex; i++)
+    {
+      stopIndex = i;
+
+      if (!node.Children.TryGetValue(stream[i], out TrieNode? value))
+      {
+        return false;
+      }
+
+      if (node.IsWord)
+      {
+        return true;
+      }
+
+      node = value;
+    }
+
+    return node.IsWord;
+  }
+
+  public int CountOccurrences(string stream)
+  {
+    int counter = 0;
+    for (int i = 0; i < stream.Length;)
+    {
+      var current = stream.Substring(i, stream.Length);
+      if (CountOnStream(current, 0, current.Length, out i))
+      {
+        counter++;
+      }
+    }
+
+    return counter;
   }
 }
--- WordFinder/TrieNode.cs
+++ WordFinder/TrieNode.cs
@@ -1,7 +1,13 @@
+using WordFinder.Interfaces;
+
 namespace WordFinder;
 
-public class TrieNode
+public class TrieNode //: ITrieNode<string>
 {
   public bool IsWord { get; set; }
   public Dictionary<char, TrieNode> Children { get; } = [];
+
+  // public string Payload { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+  // public Dictionary<char, ITrieNode<string>> Children => [];
 }
