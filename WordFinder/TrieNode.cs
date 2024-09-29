8c0e46710f65a39b5789baaa82432abff680dd44
temobgallardo <62340961+temobgallardo@users.noreply.github.com>
2024-09-29 16:26:40 -0600

    added extension method to get node

--- WordFinder/TrieNode.cs
+++ WordFinder/TrieNode.cs
@@ -7,6 +7,7 @@ public class TrieNode //: ITrieNode<string>
   public bool IsWord { get; set; }
   public Dictionary<char, TrieNode> Children { get; } = [];
 
+  public bool TryGetNode(char prefix, out TrieNode? node) => Children.TryGetValue(prefix, out node);
   // public string Payload { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
   // public Dictionary<char, ITrieNode<string>> Children => [];
