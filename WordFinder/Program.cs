6a288eb454758fef9b84a46deb67856151a39794
temobgallardo <62340961+temobgallardo@users.noreply.github.com>
2024-10-02 11:19:05 -0600

    setting the worker and running it

--- WordFinder/Program.cs
+++ WordFinder/Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("The current time is " + DateTime.Now);
+﻿using Serilog;
+using WordFinder;
+using WordFinder.Interfaces;
+using WordFinder.MockServices;
+using WordFinder.Workers;
+
+Log.Logger = new LoggerConfiguration()
+#if DEBUG
+      .WriteTo.Console()
+#else
+     .WriteTo.File(
+        Path.Combine(AppDomain.CurrentDomain.BaseDirectory, nameof(WordFinderWorker) + "-service.log")
+    )
+#endif
+    .CreateLogger();
+
+var host = Host.CreateDefaultBuilder(args)
+    .UseSerilog()
+    .ConfigureServices(services =>
+    {
+      services.AddSingleton<IRequestMatrixService, RequestMatrixService>();
+      services.AddSingleton<IRequestWordsToSearchService, RequestWordsToSearchService>();
+      services.AddSingleton<ITrie<string>, Trie>();
+      services.AddScoped<IWordFinder, WordFinder.WordFinder>();
+      services.AddHostedService<WordFinderWorker>();
+    })
+    .Build();
+
+await host.RunAsync();
