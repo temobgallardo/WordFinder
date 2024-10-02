e87a46640176883be9ccda078daf41bc788f577a
temobgallardo <62340961+temobgallardo@users.noreply.github.com>
2024-10-02 15:29:44 -0600

    fixed tests and create more

--- WordFinder.Tests/WordFinder_Tests.cs
+++ WordFinder.Tests/WordFinder_Tests.cs
@@ -1,5 +1,8 @@
 using System.Text;
 using AutoFixture;
+using AutoFixture.AutoMoq;
+using Moq;
+using WordFinder.Interfaces;
 using WordFinder.Tests.Generators;
 
 namespace WordFinder.Tests;
@@ -20,12 +23,13 @@ public class WordFinder_Tests
   }
 
   [Fact]
-  public void FindTopTenWords_Test1()
+  public async void FindTopTenWords_Test1()
   {
-    Fixture dataCreator = new();
-    dataCreator.Customizations.Add(new CustomStringGenerator("abcdefghijklmnopqrstuvwxyz", 64));
-    List<string> matrix = dataCreator.CreateMany<string>(64).ToList();
-    WordFinder sut = new(matrix);
+    var fixture = new Fixture()
+            .Customize(new AutoMoqCustomization());
+
+    fixture.Customizations.Add(new CustomStringGenerator("abcdefghijklmnopqrstuvwxyz", 64));
+    List<string> matrix = fixture.CreateMany<string>(64).ToList();
     string[] words = ["tri", "tried", "try", "zz", "zood", "zoom", "we", "are", "humanZ"];
 
     Random wordIndexRamdomizer = new();
@@ -36,32 +40,43 @@ public class WordFinder_Tests
       var wordToInsert = words[toGet];
       int toPut = wordIndexRamdomizer.Next(64);
 
-      // Normilize toPut index so it does not put wordToGet in the row out of its range
+      // Making sure toPut doesn't go out of range
       toPut = toPut + wordToInsert.Length > row.Length ? toPut - wordToInsert.Length : toPut;
 
       StringBuilder builder = new(row);
-      String toReplace = row[toPut..(toPut + wordToInsert.Length)];
+      string toReplace = row[toPut..(toPut + wordToInsert.Length)];
       builder.Replace(toReplace, wordToInsert);
       matrix[i] = builder.ToString();
     }
 
-    var actual = sut.Find(words);
+    var requestMatrixService = fixture.Freeze<Mock<IRequestMatrixService>>();
+    requestMatrixService.Setup(self => self.GetMatrix(64)).ReturnsAsync(matrix);
+    WordFinder sut = new(requestMatrixService.Object, new Trie());
+
+    var actual = await sut.Find(words);
     Assert.True(actual.Any());
   }
 
   [Fact]
-  public void FindTopTenWords_Test2()
+  public async void FindTopTenWords_Test2()
   {
-    Fixture dataCreator = new();
-    dataCreator.Customizations.Add(new CustomStringGenerator("abcdefghijklmnopqrstuvwxyz", 64));
-    List<string> matrix = dataCreator.CreateMany<string>(64).ToList();
-    WordFinder sut = new(matrix);
+    var fixture = new Fixture()
+            .Customize(new AutoMoqCustomization());
+
+    fixture.Customizations.Add(new CustomStringGenerator("abcdefghijklmnopqrstuvwxyz", 64));
+    List<string> matrix = fixture.CreateMany<string>(64).ToList();
+
     // 8 words
     string[] words = ["tri", "tried", "try", "zood", "zoom", "we", "are", "humanZ"];
 
     PrepareMatrixToTest(matrix, words);
 
-    var actual = sut.Find(words);
+    var requestMatrixService = fixture.Freeze<Mock<IRequestMatrixService>>();
+    requestMatrixService.Setup(self => self.GetMatrix(64)).ReturnsAsync(matrix);
+    WordFinder sut = new(requestMatrixService.Object, new Trie());
+
+    var actual = await sut.Find(words);
+
     Assert.True(actual.Any());
     // If number of words changes then this assert must change
     Assert.True(actual.Count() == words.Length);
@@ -72,18 +87,23 @@ public class WordFinder_Tests
   }
 
   [Fact]
-  public void FindTopTenWords_Test3()
+  public async void FindTopTenWords_Test3()
   {
-    Fixture dataCreator = new();
-    dataCreator.Customizations.Add(new CustomStringGenerator("abcdefghijklmnopqrstuvwxyz", 64));
-    List<string> matrix = dataCreator.CreateMany<string>(64).ToList();
-    WordFinder sut = new(matrix);
+    var fixture = new Fixture()
+            .Customize(new AutoMoqCustomization());
+
+    fixture.Customizations.Add(new CustomStringGenerator("abcdefghijklmnopqrstuvwxyz", 64));
+    List<string> matrix = fixture.CreateMany<string>(64).ToList();
     // 14 words
     string[] words = ["tri", "tried", "try", "zood", "zoom", "we", "the", "humanz", "but", "robots", "are", "thinking", "of", "gods"];
-
     PrepareMatrixToTest(matrix, words);
 
-    var actual = sut.Find(words);
+    var requestMatrixService = fixture.Freeze<Mock<IRequestMatrixService>>();
+    requestMatrixService.Setup(self => self.GetMatrix(64)).ReturnsAsync(matrix);
+    WordFinder sut = new(requestMatrixService.Object, new Trie());
+
+    var actual = await sut.Find(words);
+
     Assert.True(actual.Any());
     Assert.Contains(words[0], actual, StringComparer.InvariantCultureIgnoreCase);
     Assert.Contains(words[1], actual, StringComparer.InvariantCultureIgnoreCase);
@@ -96,30 +116,29 @@ public class WordFinder_Tests
   }
 
   [Fact]
-  public void FindTopTenWords_Test4()
+  public async void FindTopTenWords_Test4()
   {
     string[] words = ["tried", "humanz", "robots", "thinking"];
     int ocurrences = 4;
     int matrixRowLenght = 64;
     DatabaseMatrixStringGenerator matrixGen = new(words, ocurrences, _alphabet: "abcdefghijklmnopqrstuvwxyz".ToCharArray(), matrixRowLenght);
-    // For assertions to work make sure that words.Lenght*2*ocurrences <= matrixRowLenght
-    // otherwise your test won't work as there are less ocurrences of the word you are asserting
-    // Due to the way the data is generated.
     var matrix = matrixGen.GenerateStrings(words.Length);
-    WordFinder sut = new(matrix);
 
-    var actual = sut.Find(words);
+    var fixture = new Fixture()
+            .Customize(new AutoMoqCustomization());
+
+    var requestMatrixService = fixture.Freeze<Mock<IRequestMatrixService>>();
+    requestMatrixService.Setup(self => self.GetMatrix(64)).ReturnsAsync(matrix);
+    WordFinder sut = new(requestMatrixService.Object, new Trie());
+
+    var actual = await sut.Find(words);
 
     Assert.True(actual.Any());
-    Assert.Contains(words[0], actual, StringComparer.InvariantCultureIgnoreCase);
-    Assert.Contains(words[1], actual, StringComparer.InvariantCultureIgnoreCase);
-    Assert.Contains(words[2], actual, StringComparer.InvariantCultureIgnoreCase);
-    Assert.Contains(words[3], actual, StringComparer.InvariantCultureIgnoreCase);
     Assert.DoesNotContain("hello", actual, StringComparer.InvariantCultureIgnoreCase);
   }
 
   [Fact]
-  public void FindTopTenWords_Test5()
+  public async void FindTopTenWords_On_Matrix()
   {
     string[] words = ["tried", "humanz", "robots", "thinking", "hi", "love", "peace",
     "carrot", "train", "deepsea", "space", "wonderful"];
@@ -131,9 +150,14 @@ public class WordFinder_Tests
       "dsfdhumalovenzjgdfiokapeacepdsgfdhpjifdeepseapkfdgfn",
       "kdupicarrotj[apokf8trainu4humanz2309irqw][aksd]"
     ];
-    WordFinder sut = new(matrix);
 
-    var actual = sut.Find(words);
+    var fixture = new Fixture()
+            .Customize(new AutoMoqCustomization());
+    var requestMatrixService = fixture.Freeze<Mock<IRequestMatrixService>>();
+    requestMatrixService.Setup(self => self.GetMatrix(64)).ReturnsAsync(matrix);
+    WordFinder sut = new(requestMatrixService.Object, new Trie());
+
+    var actual = await sut.Find(words);
 
     Assert.True(actual.Any());
     Assert.Contains(words[0], actual, StringComparer.InvariantCultureIgnoreCase);
@@ -149,6 +173,103 @@ public class WordFinder_Tests
     Assert.DoesNotContain("hello", actual, StringComparer.InvariantCultureIgnoreCase);
     Assert.DoesNotContain(words[10], actual, StringComparer.InvariantCultureIgnoreCase);
     Assert.DoesNotContain(words[11], actual, StringComparer.InvariantCultureIgnoreCase);
+  }
+
+  [Fact]
+  public async void If_NoWords_InMatrix_Then_EmptyResult()
+  {
+    string[] words = ["train", "space", "wonderful"];
+    List<string> matrix = [
+      "jgfuhumanziefnfjiooftriedkfijoosfjrobotsosdfkojfsdd",
+      "gphimkgjithinkingodgmlkgfdfibfhumanzkoopda",
+      "fsjhumanzfsdsflfsdmksdmfsd",
+      "aiokpddlmfljhihihuhumanzrjg",
+      "dsfdhumalovenzjgdfiokapeacepdsgfdhpjifdeepseapkfdgfn",
+      "kdupicarrotj[apokf8grhgfhu4humanz2309irqw][aksd]"
+    ];
+
+    var fixture = new Fixture()
+            .Customize(new AutoMoqCustomization());
+    var requestMatrixService = fixture.Freeze<Mock<IRequestMatrixService>>();
+    requestMatrixService.Setup(self => self.GetMatrix(64)).ReturnsAsync(matrix);
+    WordFinder sut = new(requestMatrixService.Object, new Trie());
+
+    var actual = await sut.Find(words);
+
+    Assert.False(actual.Any());
+  }
+
+  [Fact]
+  public async void If_NoWords_Then_EmptyResult()
+  {
+    string[] words = [];
+    List<string> matrix = [
+      "jgfuhumanziefnfjiooftriedkfijoosfjrobotsosdfkojfsdd",
+      "gphimkgjithinkingodgmlkgfdfibfhumanzkoopda",
+      "fsjhumanzfsdsflfsdmksdmfsd",
+      "aiokpddlmfljhihihuhumanzrjg",
+      "dsfdhumalovenzjgdfiokapeacepdsgfdhpjifdeepseapkfdgfn",
+      "kdupicarrotj[apokf8grhgfhu4humanz2309irqw][aksd]"
+    ];
+
+    var fixture = new Fixture()
+            .Customize(new AutoMoqCustomization());
+    var requestMatrixService = fixture.Freeze<Mock<IRequestMatrixService>>();
+    requestMatrixService.Setup(self => self.GetMatrix(64)).ReturnsAsync(matrix);
+    WordFinder sut = new(requestMatrixService.Object, new Trie());
+
+    var actual = await sut.Find(words);
+
+    Assert.False(actual.Any());
+  }
+
+  [Fact]
+  public async void If_NoWords_NorMatrix_Then_EmptyResult()
+  {
+    string[] words = [];
+    List<string> matrix = [];
+
+    var fixture = new Fixture()
+            .Customize(new AutoMoqCustomization());
+    var requestMatrixService = fixture.Freeze<Mock<IRequestMatrixService>>();
+    requestMatrixService.Setup(self => self.GetMatrix(64)).ReturnsAsync(matrix);
+    WordFinder sut = new(requestMatrixService.Object, new Trie());
+
+    var actual = await sut.Find(words);
+
+    Assert.False(actual.Any());
+  }
+
+  [Fact]
+  public async void If_NullWords_NorMatrix_Then_EmptyResult()
+  {
+    string[]? words = null;
+    List<string> matrix = [];
+
+    var fixture = new Fixture()
+            .Customize(new AutoMoqCustomization());
+    var requestMatrixService = fixture.Freeze<Mock<IRequestMatrixService>>();
+    requestMatrixService.Setup(self => self.GetMatrix(64)).ReturnsAsync(matrix);
+    WordFinder sut = new(requestMatrixService.Object, new Trie());
+
+    var actual = await sut.Find(words);
+
+    Assert.False(actual.Any());
+  }
+
+  [Fact]
+  public void If_NullMatrix_Then_NullReferenceException()
+  {
+    string[]? words = ["a", "b"];
+    List<string> matrix = null;
+
+    var fixture = new Fixture()
+            .Customize(new AutoMoqCustomization());
+    var requestMatrixService = fixture.Freeze<Mock<IRequestMatrixService>>();
+    requestMatrixService.Setup(self => self.GetMatrix(64)).ReturnsAsync(matrix);
+    WordFinder sut = new(requestMatrixService.Object, new Trie());
+
+    Assert.ThrowsAsync<NullReferenceException>(async () => await sut.Find(words));
   }
 
   private static void PrepareMatrixToTest(List<string> matrix, string[] words)
