b8c0568b587f3efdfd98c8b798926a741695eaa9
temobgallardo <62340961+temobgallardo@users.noreply.github.com>
2024-10-02 12:15:22 -0600

    now words are choosed randomly

--- WordFinder/MockServices/RequestWordsToSearchService.cs
+++ WordFinder/MockServices/RequestWordsToSearchService.cs
@@ -4,8 +4,22 @@ namespace WordFinder.MockServices;
 
 public class RequestWordsToSearchService : IRequestWordsToSearchService
 {
-  private static readonly string[] words = ["lets", "find", "some", "words", "thinking", "hi", "love", "peace",
+  private static readonly string[] words = ["lets", "find", "some", "words", "choose", "words", "to", "add", "tried", "humanz", "robots", "thinking", "hi", "love", "peace",
     "carrot", "train", "deepsea", "space", "wonderful"];
 
-  public async Task<IEnumerable<string>> GetWords() => await Task.FromResult(words);
+  public async Task<IEnumerable<string>> GetWords()
+  {
+    return await Task.FromResult(GetRandomWords());
+  }
+
+  private static IEnumerable<string> GetRandomWords()
+  {
+    Random random = new();
+    var numberOfWordsToSearch = random.Next(words.Length);
+    for (int i = 0; i < numberOfWordsToSearch; i++)
+    {
+      var index = random.Next(words.Length);
+      yield return words[index];
+    }
+  }
 }
